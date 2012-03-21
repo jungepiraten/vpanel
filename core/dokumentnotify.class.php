53b0a68a5b6cab4bcc105699aea60b6d589e7b0a
Patrick Rauscher <prauscher@ohai.su>
2012-03-21 15:20:21 +0000

    bugfix

--- core/dokumentnotify.class.php
+++ core/dokumentnotify.class.php
@@ -141,7 +141,7 @@ class DokumentNotify extends StorageClass {
 		if ($this->getEMail() != null) {
 			$mail = $config->createMail($this->getEMail());
 			$mail->setHeader("Subject", "[VPanel] Dokument");
-			$mail->setHeader("Message-ID", "dokumentnotify-" . $this->$notiz->getNotizID() . "-" . $this->getDokumentNotifyID() . "@" . $config->getHostPart());
+			$mail->setHeader("Message-ID", "dokumentnotify-" . $notiz->getDokumentNotizID() . "-" . $this->getDokumentNotifyID() . "@" . $config->getHostPart());
 			$mail->setHeader("References", "dokumentnotify-" . $dokument->getFirstNotiz()->getDokumentNotizID() . "-" . $this->getDokumentNotifyID() . "@" . $config->getHostPart());
 			$mail->setBody(<<<EOT
 Hallo,
