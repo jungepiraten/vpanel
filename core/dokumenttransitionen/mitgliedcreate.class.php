c47d33eb22825364cedc68ee76f5eba73434940c
Patrick Rauscher <prauscher@ohai.su>
2012-11-18 17:24:43 +0000

    add MitgliedTemplateChooserDokument

--- core/dokumenttransitionen/mitgliedcreate.class.php
+++ core/dokumenttransitionen/mitgliedcreate.class.php
@@ -18,4 +18,23 @@ class MitgliedCreateDokumentTransition extends StaticDokumentTransition implemen
 	}
 }
 
+class DynamicMitgliedCreateDokumentTransition extends StaticDokumentTransition implements SingleDokumentTransition {
+	public function __construct($transitionid, $label, $permission, $gliederungid, $kategorieid, $statusid, $nextkategorieid, $nextstatusid, $kommentar, $mitgliedtemplatechooser) {
+		parent::__construct($transitionid, $label, $permission, $gliederungid, $kategorieid, $statusid, $nextkategorieid, $nextstatusid, $kommentar);
+		$this->mitgliedtemplatechooser = $mitgliedtemplatechooser;
+	}
+
+	public function execute($config, $session, $dokumentid) {
+		$dokument = $session->getStorage()->getDokument($dokumentid);
+		return array("redirect" => $session->getLink("dokumente_mitglied_create", $dokumentid, $this->mitgliedtemplatechooser->getMitgliedTemplateID($session->getStorage(), $dokument)));
+	}
+
+	public function show($config, $session, $process) {
+	}
+}
+
+interface MitgliedTemplateChooserDokument {
+	public function getMitgliedTemplateID($storage, $dokument);
+}
+
 ?>
