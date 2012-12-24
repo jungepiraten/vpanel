133c697dd2aec2f36c14f55b2d1c2539c2d79a89
Patrick Rauscher <prauscher@ohai.su>
2012-12-24 22:41:51 +0000

    allow templates in mitgliedlink-dokumenttransition-kommentar

--- core/processes/dokumenttransitionmitgliedlink.class.php
+++ core/processes/dokumenttransitionmitgliedlink.class.php
@@ -5,6 +5,8 @@ require_once(VPANEL_PROCESSES . "/dokumenttransition.class.php");
 class DokumentTransaktionMitgliedLinkProcess extends DokumentTransitionProcess {
 	private $mitgliedid;
 
+	private $mitglied;
+
 	public static function factory(Storage $storage, $row) {
 		$process = parent::factory($storage, $row);
 		$process->setMitgliedID($row["mitgliedid"]);
@@ -16,13 +18,32 @@ class DokumentTransaktionMitgliedLinkProcess extends DokumentTransitionProcess {
 	}
 
 	public function setMitgliedID($mitgliedid) {
+		if ($mitgliedid != $this->mitgliedid) {
+			$this->mitglied = null;
+		}
 		$this->mitgliedid = $mitgliedid;
+	}
+
+	public function getMitglied() {
+		if ($this->mitglied == null) {
+			$this->mitglied = $this->getStorage()->getMitglied($this->getMitgliedID());
+		}
+		return $this->mitglied;
+	}
+
+	public function setMitglied($mitglied) {
+		$this->setMitgliedID($mitglied->getMitgliedID());
+		$this->mitglied = $mitglied;
 	}
 
 	public function getData() {
 		$data = parent::getData();
 		$data["mitgliedid"] = $this->mitgliedid;
 		return $data;
+	}
+
+	public function getNotizKommentar() {
+		return $this->getMitglied()->replaceText(parent::getNotizKommentar());
 	}
 
 	public function initProcess() {
