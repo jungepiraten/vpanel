3e8e45da61fd45c64dcabd7045864b13f487d9b9
Patrick Rauscher <prauscher@ohai.su>
2013-05-21 23:50:56 +0000

    pre-alpha-version of api/mitglied.php

---
+++ api/mitglied.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../config.inc.php");
+
+$session = $config->getSession(true);
+$api = $session->getTemplate();
+
+if (!$session->isSignedIn()) {
+	$api->output(array("failed" => "AUTH_MISSING"), 401);
+	exit;
+}
+
+$mitglied = $session->getStorage()->getMitglied($session->getVariable("mitgliedid"));
+
+if ($mitglied == null) {
+	$api->output(array("failed" => "MITGLIED_MISSING"), 400);
+	exit;
+}
+
+if (!$session->isAllowed("mitglieder_details", $mitglied->getLatestRevision()->getGliederungID())) {
+	$api->output(array("failed" => "PERMISSION_DENIED"), 403);
+	exit;
+}
+
+$api->output(array("mitglied" => $mitglied));
+
+?>
--- ui/apitemplate.class.php
+++ ui/apitemplate.class.php
@@ -7,6 +7,73 @@ class APITemplate {
 		$this->session = $session;
 	}
 
+	private function parseGliederung($gliederung) {
+		$g = array();
+		$g["gliederungid"] = $gliederung->getGliederungID();
+		$g["label"] = $gliederung->getLabel();
+		return $g;
+	}
+
+	private function parseUser($user) {
+		$u = array();
+		return $u;
+	}
+
+	private function parseMitgliedBeitragList($rows) {
+		return array_map(array($this, 'parseMitgliedBeitrag'), $rows);
+	}
+
+	private function parseMitgliedBeitrag($mitgliedbeitrag) {
+		$mb = array();
+		$mb["mitgliedbeitragid"] = $mitgliedbeitrag->getMitgliederBeitragID();
+		$mb["beitrag"] = $this->parseBeitrag($mitgliedbeitrag->getBeitrag());
+		$mb["hoehe"] = $mitgliedbeitrag->getHoehe();
+		$mb["buchungen"] = $this->parseMitgliedBeitragBuchungList($mitgliedbeitrag->getBuchungen());
+		return $mb;
+	}
+
+	private function parseMitgliedBeitragBuchungList($rows) {
+		return array_map(array($this, 'parseMitgliedBeitragBuchung'), $rows);
+	}
+
+	private function parseMitgliedBeitragBuchung($buchung) {
+		$b = array();
+		$b["buchungid"] = $buchung->getBuchungID();
+		$b["gliederung"] = $this->parseGliederung($buchung->getGliederung());
+		if ($buchung->getUser() != null) {
+			$b["user"] = $this->parseUser($buchung->getUser());
+		}
+		if ($buchung->getTimestamp() != null) {
+			$b["timestamp"] = $buchung->getTimestamp();
+		}
+		$b["hoehe"] = $buchung->getHoehe();
+		return $b;
+	}
+
+	private function parseMitgliedRevision($revision) {
+		$r = array();
+		$r["gliederung"] = $this->parseGliederung($revision->getGliederung());
+		if ($mitglied->isNatPerson()) {
+			$r["natperson"] = array();
+		}
+		if ($mitglied->isJurPerson()) {
+			$r["jurperson"] = array();
+		}
+		return $r;
+	}
+
+	private function parseMitglied($mitglied) {
+		$m = array();
+		$m["mitgliedid"] = $mitglied->getMitgliedID();
+		$m["eintritt"] = $mitglied->getEintrittsdatum();
+		if ($mitglied->isAusgetreten()) {
+			$row["austritt"] = $mitglied->getAustrittsdatum();
+		}
+		$m["beitraege"] = $this->parseMitgliedBeitragList($mitglied->getBeitragList());
+		$m["latest"] = $this->parseMitgliedRevision($mitglied->getLatestRevision());
+		return $m;
+	}
+
 	public function output($result = null, $httpcode = 200) {
 		$data = array();
 		if ($this->session->isActive()) {
