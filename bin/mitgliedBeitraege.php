1b520bc7bf701d991f516fcc5305c60101e01c11
Patrick Rauscher <prauscher@ohai.su>
2013-06-11 15:26:18 +0000

    do not add new beitrag if there is another beitrag for this time

--- bin/mitgliedBeitraege.php
+++ bin/mitgliedBeitraege.php
@@ -6,12 +6,23 @@ $storage = $config->getStorage();
 require_once(VPANEL_MITGLIEDERMATCHER . "/logic.class.php");
 require_once(VPANEL_MITGLIEDERMATCHER . "/ausgetreten.class.php");
 
+$beitraege = array();
+foreach ($storage->getBeitragTimeFormatList() as $beitragtimeformat) {
+	$beitraege[$beitragtimeformat->getBeitragTimeFormatID()] = $beitragtimeformat->getBeitrag()->getBeitragID();
+}
+
 $mitglieder = $storage->getMitgliederResult(new NotMitgliederMatcher(new AusgetretenMitgliederMatcher()));
 while ($mitglied = $mitglieder->fetchRow()) {
-	$beitragtimeformat = $mitglied->getLatestRevision()->getBeitragTimeFormat();
-	$beitrag = $beitragtimeformat->getBeitrag();
-	$mitgliedbeitrag = $mitglied->getBeitrag($beitrag->getBeitragID());
-	if ($mitgliedbeitrag->getHoehe() == null) {
+	$hasBeitrag = false;
+	foreach ($beitraege as $beitragid) {
+		if ($mitglied->hasBeitrag($beitragid)) {
+			$hasBeitrag = true;
+		}
+	}
+
+	if (! $hasBeitrag) {
+		$beitragid = $beitraege[$mitglied->getLatestRevision()->getBeitragTimeFormatID()];
+		$mitgliedbeitrag = $mitglied->getBeitrag($beitragid);
 		$mitgliedbeitrag->setHoehe($mitglied->getLatestRevision()->getBeitrag());
 		$mitglied->save();
 	}
