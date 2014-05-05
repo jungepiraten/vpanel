19bdcd4d56068b862c0f6acde034993c5042c9b4
Patrick Rauscher <prauscher@ohai.su>
2014-05-05 02:48:58 +0200

    add SelectPrefixDokumentTemplate

--- core/dokumenttemplates/selectprefixdate.class.php
+++ core/dokumenttemplates/selectprefixdate.class.php
@@ -1,15 +1,13 @@
 <?php
 
-require_once(VPANEL_DOKUMENTTEMPLATES . "/identifier.class.php");
+require_once(VPANEL_DOKUMENTTEMPLATES . "/selectprefix.class.php");
 
-class SelectPrefixDateDokumentTemplate extends IdentifierDokumentTemplate {
-	private $prefixes;
+class SelectPrefixDateDokumentTemplate extends SelectPrefixDokumentTemplate {
 	private $dateFormat;
 	private $dateField;
 
 	public function __construct($templateid, $label, $permissoin, $gliederungid, $prefixes, $dateFormat, $dateField, $identifierNumberLength = 3) {
-		parent::__construct($templateid, $label, $permissoin, $gliederungid, $identifierNumberLength);
-		$this->prefixes = $prefixes;
+		parent::__construct($templateid, $label, $permissoin, $gliederungid, $prefixes, $identifierNumberLength);
 		$this->dateFormat = $dateFormat;
 		$this->dateField = $dateField;
 	}
@@ -18,52 +16,16 @@ class SelectPrefixDateDokumentTemplate extends IdentifierDokumentTemplate {
 		return $this->dateField;
 	}
 
-	public function getPrefixOptions() {
-		return array_map(create_function('$a', 'return $a["label"];'), $this->prefixes);
-	}
-
 	private function getTimestamp($session) {
 		return $session->getTimestampVariable("timestamp");
 	}
 
-	private function getSelectedOption($session, $value) {
-		return $this->prefixes[$session->getVariable("option")][$value];
-	}
-
-	private function getSelectedPrefix($session) {
-		return $this->getSelectedOption($session, "prefix");
-	}
-
 	protected function getIdentifierPrefix($session) {
-		return $this->getSelectedPrefix($session) . date($this->dateFormat, $this->getTimestamp($session));
-	}
-
-	public function getDokumentKategorieID($session) {
-		return $this->getSelectedOption($session, "kategorieid");
-	}
-
-	public function getDokumentStatusID($session) {
-		return $this->getSelectedOption($session, "statusid");
-	}
-
-	public function getDokumentFlags($session) {
-		return $this->getSelectedOption($session, "flags");
+		return $this->getSelectedOption($session, "prefix") . date($this->dateFormat, $this->getTimestamp($session));
 	}
 
 	public function getDokumentLabel($session) {
 		return $session->getVariable("label");
-	}
-
-	public function getDokumentFile($session) {
-		return $session->getFileVariable("file");
-	}
-
-	public function getDokumentData($session) {
-		return array();
-	}
-
-	public function getDokumentKommentar($session) {
-		return $session->getVariable("kommentar");
 	}
 }
 
